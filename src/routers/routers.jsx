111cf33457cda4de74d9061ec5c0f16a32891f76
Maria Fernanda <maria.ibanez.leiva@gmail.com>
2021-11-15 18:11:56 -0300

    Add file versel.json

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,4 +1,4 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import { About } from "../components/about";
 import Contact from "../components/contact";
@@ -7,13 +7,13 @@ import Nav from "../components/nav";
 
 export const Routers = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Nav />
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/" element={<Home />} />
       </Routes>                  
-    </HashRouter>
+    </BrowserRouter>
   )
 }
