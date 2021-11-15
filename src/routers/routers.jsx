6aa6ec505b4e90edd26094b3701bf4761bf879a5
Maria Fernanda <maria.ibanez.leiva@gmail.com>
2021-11-15 17:45:26 -0300

    Change to HashRouter

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, Link } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { About } from "../components/about";
 import Contact from "../components/contact";
 import Home from "../components/home";
@@ -7,13 +7,13 @@ import Nav from "../components/nav";
 
 export const Routers = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Nav />
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/" element={<Home />} />
       </Routes>                  
-    </BrowserRouter>
+    </HashRouter>
   )
 }
